967de3b2a7438ae1f078165c0ac1c0a7292f72c4
anamikamanish10 <148894962+anamikamanish10@users.noreply.github.com>
2023-12-02 23:54:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
+
 # Import necessary libraries
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
 
 # Set the title of the dashboard
-st.title('Advanced Streamlit App')
+st.title('Advanced Streamlit App (No Plotly)')
 
 # Add a file uploader to allow the user to upload a CSV file
 uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
@@ -20,9 +21,15 @@ if uploaded_file is not None:
     # Allow the user to select columns for plotting
     selected_columns = st.multiselect('Select Columns for Plotting', df.columns)
 
-    # Plot the selected columns using Plotly Express
+    # Plot the selected columns using Matplotlib
     if selected_columns:
-        fig = px.line(df, x='Date', y=selected_columns, title='Customizable Plot')
-        st.plotly_chart(fig)
+        for column in selected_columns:
+            plt.plot(df[column], label=column)
+
+        plt.title('Customizable Plot')
+        plt.xlabel('Index')
+        plt.ylabel('Values')
+        plt.legend()
+        st.pyplot(plt)
 else:
     st.write('Upload a CSV file to get started.')
