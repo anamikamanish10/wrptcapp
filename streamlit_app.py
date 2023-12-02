a544684011fde517402de7bbd174c7969c61e7e4
anamikamanish10 <148894962+anamikamanish10@users.noreply.github.com>
2023-12-02 23:27:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,8 @@
-# Import necessary libraries
+# Import necessary library
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
-
-# Create a sample dataframe
-data = {
-    'X': np.random.rand(100),
-    'Y': np.random.rand(100),
-}
-
-df = pd.DataFrame(data)
 
 # Set the title of the dashboard
-st.title('Simple Streamlit App')
+st.title('My First Streamlit App')
 
-# Display the raw data
-st.write('### Raw Data')
-st.write(df)
-
-# Create a scatter plot using Plotly Express
-scatter_plot = px.scatter(df, x='X', y='Y', title='Scatter Plot')
-
-# Display the scatter plot
-st.plotly_chart(scatter_plot)
+# Display a simple message
+st.write('Hello, Streamlit!')
