48fbfd5457748dc839af9f6eeabbb9f510976354
anamikamanish10 <148894962+anamikamanish10@users.noreply.github.com>
2023-12-02 23:47:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,28 @@
-# Import necessary library
+# Import necessary libraries
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 # Set the title of the dashboard
-st.title('Moderate Streamlit App')
+st.title('Advanced Streamlit App')
 
-# Add a sidebar for user input
-st.sidebar.header('User Input')
+# Add a file uploader to allow the user to upload a CSV file
+uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
 
-# Allow the user to enter a number
-user_input = st.sidebar.number_input('Enter a number:', value=5.0)
+# If a file is uploaded, load the data into a DataFrame
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
 
-# Perform a simple calculation
-result = user_input * 2
+    # Display the raw data
+    st.write('### Raw Data')
+    st.write(df)
 
-# Display the result
-st.write(f'Twice of {user_input} is: {result}')
+    # Allow the user to select columns for plotting
+    selected_columns = st.multiselect('Select Columns for Plotting', df.columns)
+
+    # Plot the selected columns using Plotly Express
+    if selected_columns:
+        fig = px.line(df, x='Date', y=selected_columns, title='Customizable Plot')
+        st.plotly_chart(fig)
+else:
+    st.write('Upload a CSV file to get started.')
